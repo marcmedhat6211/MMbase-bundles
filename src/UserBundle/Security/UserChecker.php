da3dcee1b4757b2870cb5d68e054ee1c64c3310b
marcmedhat6211 <marcmedhat6211@gmail.com>
2022-01-01 18:23:16 +0200

    adding last login time when the user logs in and adjusting it in the admin page

--- src/UserBundle/Security/UserChecker.php
+++ src/UserBundle/Security/UserChecker.php
@@ -3,12 +3,20 @@
 namespace App\UserBundle\Security;
 
 use App\UserBundle\Entity\User as AppUser;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserChecker implements UserCheckerInterface
 {
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function checkPreAuth(UserInterface $user): void
     {
         if (!$user instanceof AppUser) {
@@ -26,9 +34,8 @@ class UserChecker implements UserCheckerInterface
             return;
         }
 
-        // user account is expired, the user may be notified
-        if (!$user->getEnabled()) {
-            throw new AccessDeniedException("Your account has been blocked for some reason, please contact administrator");
-        }
+        $user->setLastLogin(new \DateTime());
+        $this->em->persist($user);
+        $this->em->flush();
     }
 }
